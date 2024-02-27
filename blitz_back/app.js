365acd14afd6859efc6a9bad504003710e49a422
Ali2210 <Alideveloper95@gmail.com>
2024-02-27 21:25:31 +0500

    curd react express js

--- blitz_back/app.js
+++ blitz_back/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => {
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({extended : true}));
+
+app.get('/cars', (req, res) => {
 
     // these headers make sure cros bowser policy should be default 
     // when client request and return http-origin 1.1, 
@@ -12,7 +19,32 @@ app.get('/hello', (req, res) => {
 
 
     // here response should be in json
-    res.send({"message" : "hello"});    
+    res.send({"message" : {"cartype" : "Ferrai", "owner" : "Mudassiq", "date" : "02-02-2024", "model" : "sf90-stradale", "id" : "65d323cd6227a803e824d63e"}});    
+})
+
+
+app.get('/buy', (req, res) => {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');  
+
+  console.log("request :", req);
+
+  res.send({"massage" : req.params});
+
+    
+})
+
+
+app.post('/buy', (req, res) => {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');  
+
+    console.log("response body", res);
+    console.log("body", req.body);
+    
+    res.send({"message" : req.body});
 })
 
 app.listen(port, () => {
--- blitz_front/src/App.jsx
+++ blitz_front/src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 import Box from '@mui/material/Box';
 import axios from 'axios';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import AtmIcon from '@mui/icons-material/Atm';
+import { Button } from '@mui/material';
 
 
 function App() {
@@ -11,20 +14,74 @@ function App() {
   const [server, setServer] = useState([]);
   const handleReq = async () =>{
 
-    const data = (await axios.get('http://localhost:3000/hello')).data;
+    const data = (await axios.get('http://localhost:3000/cars')).data;
 
     console.log("data == ", data);
 
     setServer(data);
   }; 
 
-  handleReq();
+
+  const handleNew = async (event) => {
+
+      const cartype = event.target.elements.cartype.value;
+      const owner = event.target.elements.owner.value;
+      const date = event.target.elements.date.value;
+      const model = event.target.elements.model.value;
+
+      console.log("data == ", cartype, owner, date, model);
+
+      const data = await fetch('http://localhost:3000/buy', {
+        method : 'POST',
+        body : JSON.stringify({
+            cartype : cartype,
+            owner : owner,
+            model : model,
+            date : date,
+            id : "65d323cd6227a803e924d63a"
+        })
+      });
+      
+      const resp = await data.json();
+      console.log("response {:?}", resp);
+
+      event.preventDefault();
+
+  }
 
   return (
-    
-    <Box sx={{color : 'white'}}>
-      <span> expressJS response == </span>  {JSON.stringify(server.message)}
-    </Box>
+    <Box>
+        <Button variant='normal' onClick={handleReq}>
+          <DirectionsCarIcon sx={{color : 'black'}}></DirectionsCarIcon>
+        </Button>
+        <Box sx={{position : 'relative', top : '1pc', left : '0pc', color : 'black'}}>
+          cartype : {JSON.stringify(server.message)},
+        </Box>
+        {/* <Box sx={{position : 'relative', top : '1.5pc', left : '0pc'}}>
+          owner : {JSON.stringify(server.message.owner)},
+        </Box>
+        <Box sx={{position : 'relative', top : '2pc', left : '0pc'}}>
+          buy date : {JSON.stringify(server.message.date)},
+        </Box>
+        <Box sx={{position : 'relative', top : '2.5pc', left : '0pc'}}>
+          car model : {JSON.stringify(server.message.model)},
+        </Box>
+        <Box sx={{position : 'relative', top : '3pc', left : '0pc'}}>
+          car issue id : {JSON.stringify(server.message.id)},
+        </Box> */}
+
+        <Box sx={{position : 'relative', top : '5pc'}}>
+          <form onSubmit={handleNew}>
+              <input type='text' id="cartype" variant="filled" name="cartype" placeholder='enter your car type [ferrai, tesla]'></input>
+              <input type='text' id="owner" variant="filled" name="owner" placeholder='enter your name'></input>
+              <input type='date' id="date" variant="filled" name='date'></input>
+              <input type='model' id="model" variant="filled" name='model'></input>
+              <Button variant='outlined' type='submit'>
+                    <AtmIcon></AtmIcon>
+              </Button>
+          </form>
+        </Box>
+    </Box>  
   )
 }
 
